f815b9bb30d6c8ea6865dcebeff158c9457b8326
streamchapuy <federicobraga1234@gmail.com>
2024-08-06 11:44:22 -0300

    Inicio del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { SorteoComponent } from './Pages/sorteo/sorteo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    SorteoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
