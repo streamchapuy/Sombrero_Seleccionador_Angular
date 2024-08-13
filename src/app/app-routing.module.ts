9fd336204475d2158e9f4eb570e169b364356dfc
Nacho <Cifuentes.Ignacio.1994@gmail.com>
2024-08-12 21:45:42 -0300

    Agregado el button y el escudo.
    Se agrego carpeta la varita de Harry.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { SorteoComponent } from './Pages/sorteo/sorteo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path:'sorteo', component: SorteoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
