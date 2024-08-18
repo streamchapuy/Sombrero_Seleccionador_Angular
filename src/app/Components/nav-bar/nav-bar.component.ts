e7625ffc302d5b2e258dbf143eaeeb7fa9da8357
streamchapuy <federicobraga1234@gmail.com>
2024-08-18 11:59:15 -0300

    Update: - Se modifico la distribucion de los componentes - Se modifico el tamaño de los escudos - Se agrego una nueva funcion a la nav-bar para desplazamiento horinzontal, ahora se puede mover con la rueda del mouse y manteniendo click

--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  isDragging = false;
+  startX: number = 0;
+  scrollLeft: number = 0;
 
+  startDrag(event: MouseEvent) {
+    this.isDragging = true;
+    const target = event.currentTarget as HTMLElement;
+    this.startX = event.pageX - target.scrollLeft;
+    this.scrollLeft = target.scrollLeft;
+  }
+
+  onDrag(event: MouseEvent) {
+    if (!this.isDragging) return;
+    const x = event.pageX - this.startX;
+    const target = event.currentTarget as HTMLElement;
+    const walk = x - this.scrollLeft; // Desplazamiento del cursor
+    target.scrollLeft = this.scrollLeft - walk;
+  }
+
+  stopDrag() {
+    this.isDragging = false;
+  }
+
+  onWhell(event: WheelEvent) {
+    const target = event.currentTarget as HTMLElement;
+    target.scrollLeft += event.deltaY;
+    event.preventDefault();
+  }
 }
